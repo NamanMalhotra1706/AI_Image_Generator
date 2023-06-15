f5340f3a7dd75193ba9dc81472efef16c2e3ba0a
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2023-06-15 16:09:42 +0530

    Server Render Link Added

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -30,7 +30,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:3000/api/v1/image', {
+        const response = await fetch('https://ai-image-generator-naman-malhotra.onrender.com/api/v1/image', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -58,7 +58,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:3000/api/v1/post', {
+        const response = await fetch('https://ai-image-generator-naman-malhotra.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
