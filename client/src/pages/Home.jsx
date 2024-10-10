9e175e95eb0a794878a7155de728d226b521e835
Naman Malhotra <naman0913.be21@chitkara.edu.in>
2024-10-10 13:36:16 +0530

    render link added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3000/api/v1/post', {
+      const response = await fetch('https://ai-image-generator-naman-malhotra.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
